db176b40bf08069783071c800f8eff938b233029
brayjmendoza <brayjmendoza@gmail.com>
2026-03-29 00:38:19 -0700

    linked treetop with xtext, but need to figure out gems

--- tools/eclipse/dev/org.xtext.udb.parent/org.xtext.udb/src/org/xtext/example/udb/treetop/TreetopParser.java
+++ tools/eclipse/dev/org.xtext.udb.parent/org.xtext.udb/src/org/xtext/example/udb/treetop/TreetopParser.java
@@ -1,21 +1,59 @@
 package org.xtext.example.udb.treetop;
 
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
+import org.eclipse.core.runtime.FileLocator;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 import org.jruby.embed.ScriptingContainer;
 import org.jruby.embed.LocalContextScope;
 
+import org.xtext.udb.jruby.JRubyBundleHelper;
+
 public class TreetopParser {
 
-	private static final TreetopParser INSTANCE = new TreetopParser();
+    private static final TreetopParser INSTANCE = new TreetopParser();
 
     private final ScriptingContainer ruby;
     private Object parser;
-    private String lastError;
 
     private TreetopParser() {
         ruby = new ScriptingContainer(LocalContextScope.SINGLETON);
-        ruby.runScriptlet("require 'treetop'");
-        ruby.runScriptlet("Treetop.load('/path/to/tools/ruby-gems/idlc/lib/idlc/idl.treetop')");
-        parser = ruby.runScriptlet("IdlParser.new");  // class name from your .treetop file
+
+        try {
+            // Point JRuby at the vendored gems dir instead of the system gem path
+            String gemsPath = JRubyBundleHelper.getGemsPath();
+            ruby.runScriptlet(
+                "Gem.paths = { 'GEM_HOME' => '" + gemsPath + "', " +
+                "              'GEM_PATH' => '" + gemsPath + "' }"
+            );
+            
+            // Resolve the .treetop grammar file
+            Bundle bundle = FrameworkUtil.getBundle(TreetopParser.class);
+            URL bundleRoot = FileLocator.toFileURL(bundle.getEntry("/"));
+            String bundleRootPath = bundleRoot.getPath();
+            String idlcPath = bundleRootPath + 
+            	    "../../../../../tools/ruby-gems/idlc/lib/idlc";
+            	idlcPath = new File(idlcPath).getCanonicalPath();	
+            	
+	        ruby.runScriptlet("$LOAD_PATH.unshift('" + idlcPath + "')");
+	        ruby.runScriptlet("require 'treetop'");
+	        ruby.runScriptlet("require '" + idlcPath + "/syntax_node.rb'");
+	        ruby.runScriptlet("require '" + idlcPath + "/ast.rb'");
+	        ruby.runScriptlet("require '" + idlcPath + "/ast_decl.rb'");
+	        ruby.runScriptlet("require '" + idlcPath + "/type.rb'");
+	        ruby.runScriptlet("require '" + idlcPath + "/interfaces.rb'");
+	        ruby.runScriptlet("require '" + idlcPath + "/symbol_table.rb'");
+	        
+	        String grammarPath = idlcPath + "/idl.treetop";
+            ruby.runScriptlet("Treetop.load('" + grammarPath + "')");
+            
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to resolve gems path", e);
+        }
+        
+        parser = ruby.runScriptlet("IdlParser.new");
     }
 
     public static TreetopParser getInstance() {
